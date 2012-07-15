c8ee71addc739b7e51beab17b6e4769f994b5700
Oliver Weiler <wo6980@googlemail.com>
2012-07-15 19:19:45 +0200

    Specs data now gets aggregated for evaluation in the report.

--- bash-specs.sh
+++ bash-specs.sh
@@ -1,23 +1,7 @@
 #/usr/bin/env bash
 
-number_of_specs=0
-
 describe() {
 	echo "$1"
-}
-
-it() {
-	before_each
-
-	local result=$(eval "$2")
-
-	if [[ $result ]]; then
-		echo "was successful."
-	else
-		echo "wasn't successful."
-	fi
-
-	after_each
 }
 
 before_each() {
@@ -28,12 +12,41 @@ after_each() {
 	:
 }
 
-execute_suites() {
-  local suites=${}
+it() {
+	before_each
 
-	for suite in "$suites"; do
+	result=$(eval "$2")
+  ((result > 0)) && specs_failed
+  specs_results+=($1 $result)
+
+	after_each
+}
+
+print_report() {
+	local spec_or_specs="spec"
+
+	((number_of_specs > 1)) && spec_or_specs="specs"
+
+	local number_of_specs=${#specs_results[@]}
+
+	echo "$number_of_specs ${spec_or_specs}, $number_of_specs_failed failed"
+
+	for spec_result in "${spec_results[@]}"; do
+
+  done
+}
+
+execute_suites() {
+	for suite in "$1"; do
 		. "$suite"
 	done
+
+  print_report
 }
+
+specs_results=()
+number_of_specs_failed=0
+
+(($# == 0)) && set -- *.suite
 
 execute_suites "$@"
