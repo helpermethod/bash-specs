5ec8f85b6a70b8808f077a7c73f1f578f21ec226
Oliver Weiler <wo6980@googlemail.com>
2015-06-11 12:13:23 +0200

    Update to-be-less-than-or-equal-to-spec.sh

--- test/matchers/to-be-less-than-or-equal-to-spec.sh
+++ test/matchers/to-be-less-than-or-equal-to-spec.sh
@@ -2,7 +2,7 @@ describe 'to_be_less_than_or_equal_to'
 
 _() {
   to_be_less_than_or_equal_to 0 1
-  expect "$?" to_equal 0
+  expect "$?" to_equal 0 || return
   to_be_less_than_or_equal_to 1 1
   expect "$?" to_equal 0
 }
