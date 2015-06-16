9d4f5f3b13f2a5106ab8cb505eb12096876c2f02
helpermethod <wo6980@gmail.com>
2015-06-16 07:31:36 +0000

    Minor fixes

--- test/matchers/to_be_less_than_or_equal_to_spec.sh
+++ test/matchers/to_be_less_than_or_equal_to_spec.sh
@@ -2,16 +2,16 @@ describe 'to_be_less_than_or_equal_to'
 
 _() {
   to_be_less_than_or_equal_to 0 0
-  expect "$?" to_equal 0
+  expect $? to_equal 0
   to_be_less_than_or_equal_to 0 1
-  expect "$?" to_equal 0
+  expect $? to_equal 0
 }
 
 it 'passes when actual <= expected' _
 
 _() {
   to_be_less_than_or_equal_to 1 0
-  expect "$?" not to_equal 0
+  expect $? not to_equal 0
 } 
 
 it 'fails when actual > expected' _
