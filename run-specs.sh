b7e3110bd99805b2484b1a58d9c8c3b346a50931
helpermethod <wo6980@gmail.com>
2015-06-18 16:09:42 +0000

    Prefix globals

--- run-specs.sh
+++ run-specs.sh
@@ -2,5 +2,6 @@
 
 cp {src,spec}/bash-specs
 
-globals=($(grep -E -e '^[a-z]+(_[a-z]+)*=' -e '^(__)?[a-z]+(_[a-z]+)*\(' spec/bash-specs))
-# TODO replace each global occurence
+globals=($(sed -n -r -e 's/^([a-z]+(_[a-z]+)*)=.*/\1/p' -e 's/^(__)?([a-z]+(_[a-z]+)*)\(.*/\2/p' spec/bash-specs))
+
+(IFS='|'; sed -i -r "s/(${globals[*]})/t_\1/g" spec/bash-specs)
