4ac1cb9479eda43cfeaf59ef3468543a4aa72f11
helpermethod <wo6980@gmail.com>
2015-06-18 16:50:24 +0000

    Add pattern for associative arrays

--- run-specs.sh
+++ run-specs.sh
@@ -1,7 +1,9 @@
-#trap 'rm -rf spec/bash-specs'
+#trap 'rm -rf spec/bash-specs' EXIT
 
 cp {src,spec}/bash-specs
 
-globals=($(sed -n -r -e 's/^([a-z]+(_[a-z]+)*)=.*/\1/p' -e 's/^(__)?([a-z]+(_[a-z]+)*)\(.*/\2/p' spec/bash-specs))
+globals=($(sed -n -r -e 's/^([a-z]+(_[a-z]+)*)=.*/\1/p' \
+                     -e 's/^ *?(__)?([a-z]+(_[a-z]+)*)\(.*/\2/p' \
+                     -e 's/^declare -A ([a-z]+(_[a-z]+)*)/\1/p' spec/bash-specs))
 
 (IFS='|'; sed -i -r "s/(${globals[*]})/t_\1/g" spec/bash-specs)
