645b6c3a1dbd9cea349d5c52e50ec6663b4458cd
helpermethod <wo6980@gmail.com>
2015-06-26 07:47:14 +0000

    Display colors only on tty

---
+++ spec/framework/xdescribe_spec.sh
@@ -0,0 +1,18 @@
+cd "${BASH_SOURCE%/*}" && . ../bash-specs
+
+describe 'xdescribe'
+
+_() {
+ expect "$(t_xdescribe 'bash')" to_equal $'\nbash'
+}
+
+it 'prints the name of the spec suite' _
+
+_() {
+  t_skip_specs=false
+  t_xdescribe 'bash'
+
+  expect "$t_skip_specs" to_equal true
+}
+
+it 'sets the skip_specs flag to true' _
