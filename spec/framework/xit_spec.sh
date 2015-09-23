c6e70af6aa7ac7f0d1f22f9d40ccaa41e92c6f50
helpermethod <wo6980@gmail.com>
2015-09-23 07:11:22 +0000

    Implement xit_spec.sh

---
+++ spec/framework/xit_spec.sh
@@ -0,0 +1,25 @@
+. "${current_dir}/../bash-specs"
+
+describe 'xit'
+
+before_each() {
+  create_mock __t_inc
+  create_mock __t_print_spec_result
+}
+
+_() {
+  t_xit 'skip'
+
+  expect __t_inc to_be_called_with "$t_number_of_specs" || return
+  expect __t_inc to_be_called_with "$t_number_of_skipped_specs"
+}
+
+it 'calls __inc with matching parameters' _
+
+_() {
+  t_xit 'skip'
+
+  expect __t_print_spec_result to_be_called_with "$t_yellow" 'skip' 0
+}
+
+it 'calls __print_spec_result with matching parameters' _
