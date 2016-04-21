111ee51f1dd5e82072d1c78a49797c9fd16c2bdf
helpermethod <wo6980@gmail.com>
2016-04-21 06:50:00 +0000

    Add spec for to_be_greater_than_or_equal_to matcher

---
+++ spec/matchers/to_be_greater_than_or_equal_to_spec.sh
@@ -0,0 +1,19 @@
+. src/bash-specs
+
+t_describe 'to_be_greater_than_or_equal_to'
+
+_() {
+  to_be_greater_than_or_equal_to 0 0
+  t_expect $? t_to_equal 0 || return
+  to_be_greater_than_or_equal_to 1 0
+  t_expect $? t_to_equal 0
+}
+
+t_it 'passes when actual >= expected' _
+
+_() {
+  to_be_greater_than_or_equal_to 0 1
+  t_expect $? not t_to_equal 0
+}
+
+t_it 'fails when actual < expected' _
